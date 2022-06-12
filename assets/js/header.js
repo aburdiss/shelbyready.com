5acb58b5ec18a142a931ec0d756b6b301d9bf3d0
Alexander Burdiss <aburdiss@icloud.com>
2022-06-12 09:35:09 -0400

    Creates Dynamic Mobile Navigation that progressively enhances static mobile navigation

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,1 +1,44 @@
-console.log("header");
+// Build hamburger navigation to replace static navigation (if JS enabled)
+const closeButton = document.createElement("button");
+closeButton.classList.add("hamburger");
+closeButton.innerHTML = `
+<span class="visually-hidden-accessibility">Open Menu</span>
+<span class="first-span"></span>
+<span class="second-span"></span>
+<span class="third-span"></span>
+`;
+const headerContent = document.querySelector("header .inner-content");
+closeButton.addEventListener("click", function () {
+  const header = document.querySelector("header");
+  const hamburgerMenu = document.querySelector(".hamburger");
+  const menuAccessibilityText = document.querySelector(
+    ".hamburger .visually-hidden-accessibility"
+  );
+  const isOpen = hamburgerMenu.classList.contains("open");
+  if (isOpen) {
+    document.body.style.overflow = "visible";
+    hamburgerMenu.classList.remove("open");
+    header.classList.remove("mobile-menu-open");
+    menuAccessibilityText.innerText = "Open Menu";
+  } else {
+    document.body.style.overflow = "hidden";
+    hamburgerMenu.classList.add("open");
+    header.classList.add("mobile-menu-open");
+    menuAccessibilityText.innerText = "Close Menu";
+  }
+});
+// Close the menu when resized larger than mobile
+window.addEventListener("resize", function (event) {
+  if (window.innerWidth >= 860) {
+    const header = document.querySelector("header");
+    const hamburgerMenu = document.querySelector(".hamburger");
+    const menuAccessibilityText = document.querySelector(
+      ".hamburger .visually-hidden-accessibility"
+    );
+    document.body.style.overflow = "visible";
+    hamburgerMenu.classList.remove("open");
+    header.classList.remove("mobile-menu-open");
+    menuAccessibilityText.innerText = "Open Menu";
+  }
+});
+headerContent.appendChild(closeButton);
--- bundle.js
+++ bundle.js
@@ -1,41 +1,7 @@
-// bundle.js created by bundlejs.sh v1.1.0 Sat Jun 11 10:11:30 EDT 2022 Copyright (c) Alexander Burdiss
-//==============================
-// ./assets/js/footer.js
-//==============================
-(function assetsjsfooterjs() {
-console.log("footer");
-})();
-//==============================
+// bundle.js created by bundlejs.sh v1.2.0 Sun Jun 12 09:35:09 EDT 2022
 // ./assets/js/header.js
-//==============================
-(function assetsjsheaderjs() {
-console.log("header");
-})();
-//==============================
+(function assetsjsheaderjs() {const closeButton = document.createElement("button");closeButton.classList.add("hamburger");closeButton.innerHTML = `<span class="visually-hidden-accessibility">Open Menu</span><span class="first-span"></span><span class="second-span"></span><span class="third-span"></span>`;const headerContent = document.querySelector("header .inner-content");closeButton.addEventListener("click", function () { const header = document.querySelector("header"); const hamburgerMenu = document.querySelector(".hamburger"); const menuAccessibilityText = document.querySelector( ".hamburger .visually-hidden-accessibility" ); const isOpen = hamburgerMenu.classList.contains("open"); if (isOpen) { document.body.style.overflow = "visible"; hamburgerMenu.classList.remove("open"); header.classList.remove("mobile-menu-open"); menuAccessibilityText.innerText = "Open Menu"; } else { document.body.style.overflow = "hidden"; hamburgerMenu.classList.add("open"); header.classList.add("mobile-menu-open"); menuAccessibilityText.innerText = "Close Menu"; }});window.addEventListener("resize", function (event) { if (window.innerWidth >= 860) { const header = document.querySelector("header"); const hamburgerMenu = document.querySelector(".hamburger"); const menuAccessibilityText = document.querySelector( ".hamburger .visually-hidden-accessibility" ); document.body.style.overflow = "visible"; hamburgerMenu.classList.remove("open"); header.classList.remove("mobile-menu-open"); menuAccessibilityText.innerText = "Open Menu"; }});headerContent.appendChild(closeButton);})();
 // ./assets/js/noticeBanner.js
-//==============================
-(function assetsjsnoticeBannerjs() {
-// Add under construction banner to every page.
-const noticeBannerExists = document.querySelector(".notice-banner");
-if (!noticeBannerExists) {
-  const noticeBanner = `
-<div class="inner-content">
-  <img src="/assets/images/warning.svg" alt="Notice" />
-  This site is still under construction. Some site features may not work as
-  intended.
-</div>`;
-  const bannerRoot = document.createElement("div");
-  bannerRoot.classList.add("notice-banner");
-  bannerRoot.innerHTML = noticeBanner;
-
-  const header = document.querySelector("header");
-  document.body.insertBefore(bannerRoot, header.nextSibling);
-}
-})();
-//==============================
+(function assetsjsnoticeBannerjs() {const noticeBannerExists = document.querySelector(".notice-banner");if (!noticeBannerExists) { const noticeBanner = `<div class="inner-content"> <img src="/assets/images/warning.svg" alt="Notice" /> This site is still under construction. Some site features may not work as intended.</div>`; const bannerRoot = document.createElement("div"); bannerRoot.classList.add("notice-banner"); bannerRoot.innerHTML = noticeBanner; const header = document.querySelector("header"); document.body.insertBefore(bannerRoot, header.nextSibling);}})();
 // ./global.js
-//==============================
-(function globaljs() {
-// Add a JS class so the CSS can take into account the new JS styles.
-document.querySelector("body").classList.add("JS-Enabled");
-})();
+(function globaljs() {document.querySelector("body").classList.add("JS-Enabled");})();
