2a374c2b56cc72414002e93a99cb3e5b5b22f3cf
Alexander Burdiss <aburdiss@icloud.com>
2022-06-20 23:10:16 -0400

    Updates JS on 2022 Just in time diesel outage workshop page so form fits on page with all errors

--- get-trained/2022-Shelby-County-Just-In-Time-Diesel-Outage-Workshop/ShelbyCountyJustInTimeDieselOutage.js
+++ get-trained/2022-Shelby-County-Just-In-Time-Diesel-Outage-Workshop/ShelbyCountyJustInTimeDieselOutage.js
@@ -5,14 +5,21 @@ function updateFormWidth() {
   if (form) {
     const width = window.innerWidth;
     if (width > 850) {
+      // Desktop
       form.width = "800";
-      form.height = "1212";
+      form.height = "1362";
     } else if (width > 450) {
+      // Tablet
       form.width = "400";
-      form.height = "1350";
+      form.height = "1501";
+    } else if (width > 370) {
+      // iPhone and other smart phone size
+      form.width = "310";
+      form.height = "1589";
     } else {
+      // Extremely small phone
       form.width = "250";
-      form.height = "1626";
+      form.height = "1777";
     }
   }
 }
