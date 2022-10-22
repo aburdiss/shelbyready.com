2031d2685639cb9c192ed85fe4d746c0bd261b5e
Alexander Burdiss <aburdiss@icloud.com>
2022-10-22 10:59:38 -0400

    Adds dynamic header navigation
    Includes all possible pages in header, and adds JS and CSS to make them
    function as as normal header.

--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
-// bundle.js created by bundlejs.sh v1.2.2 Mon Oct 10 21:44:26 EDT 2022
+// bundle.js created by bundlejs.sh v1.2.2 Sat Oct 22 11:03:14 EDT 2022
 // ./global.js
 (function globaljs() {// Add a JS class so the CSS can take into account the new JS styles.
 document.querySelector("body").classList.add("JS-Enabled");
@@ -100,11 +100,75 @@ if (!noticeBannerExists) {
 const headerNav = document.querySelector("header nav");
 headerNav.innerHTML = `
 <a href="/">Home</a>
-<a href="/get-informed/">Get Informed</a>
+<div class="navgroup" aria-expanded="false">
+  <button class="chevron" id="nav1">Get Informed</button>
+  <div class="subnav" id="subnav1">
+    <a href="/get-informed/">Overview</a>
+    <a href="/get-informed/books/">Books</a>
+    <a href="/get-informed/butchering">Butchering</a>
+    <a href="/get-informed/food-preservation">Food Preservation</a>
+    <a href="/get-informed/gardening/">Gardening</a>
+    <a href="/get-informed/guides/">How-To Guides</a>
+    <a href="/get-informed/recipes/">Recipes</a>
+  </div>
+</div>
 <a href="/get-trained/">Get Trained</a>
-<a href="/get-active/">Get Active</a>
+<div class="navgroup" aria-expanded="false">
+  <button class="chevron" id="nav3">Get Active</button>
+  <div class="subnav" id="subnav3">
+    <a href="/get-active/">Overview</a>
+    <a href="/get-active/activism/">Activism</a>
+    <a href="/get-active/churches/">Churches</a>
+    <a href="/get-active/community-events/">Community Events</a>
+    <a href="/get-active/memes/">Memes</a>
+    <a href="/get-active/small-group-resources/">Small Group Resources</a>
+  </div>
+</div>
 <a href="/about/">About</a>
 `;
+
+// Build dynamic navigation
+const navButtons = document.querySelectorAll('[id^="nav"]');
+const subnavButtons = document.querySelectorAll(".subnav");
+/**
+ * Closes open navigation elements and removes event listeners from the body
+ * @author Alexander Burdiss
+ * @since 10/22/22
+ * @version 1.0.0
+ */
+function closeOpenNavigation(event) {
+  navButtons.forEach((button) => {
+    if (button.classList.contains("open")) {
+      button.parentElement.ariaExpanded = false;
+      button.classList.remove("open");
+    }
+  });
+  subnavButtons.forEach((button) => {
+    if (button.style.display === "block") {
+      button.style.display = "none";
+    }
+  });
+  if (document.body.onclick) {
+    document.body.onclick = undefined;
+  }
+}
+navButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const number = button.id[button.id.length - 1];
+    const subnav = document.querySelector("#subnav" + number);
+    if (subnav.style.display === "block") {
+      button.parentElement.ariaExpanded = false;
+      button.classList.remove("open");
+      subnav.style.display = "none";
+    } else {
+      closeOpenNavigation();
+      button.parentElement.ariaExpanded = true;
+      button.classList.add("open");
+      subnav.style.display = "block";
+      setTimeout(() => (document.body.onclick = closeOpenNavigation), 0);
+    }
+  });
+});
 
 // Build hamburger navigation to replace static navigation (if JS enabled)
 const closeButton = document.createElement("button");
