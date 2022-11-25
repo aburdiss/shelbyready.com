866da735af90b4a6b70b22c4680cc5e326a4721c
Alexander Burdiss <aburdiss@icloud.com>
2022-11-25 18:07:36 -0500

    Add Designed by link to footer

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,28 +1,32 @@
 // Populate footer with links
-const footer = document.querySelector("footer");
+const footer = document.querySelector('footer');
 footer.innerHTML = `
 <div class="inner-content">
   <div class="left-container">
-  <h2><a href="/get-informed/">Get Informed</a></h2>
-  <a href="/get-informed/books/">Books</a>
-  <a href="/get-informed/butchering/">Butchering</a>
-  <a href="/get-informed/food-preservation">Food Preservation</a>
-  <a href="/get-informed/gardening/">Gardening</a>
-  <a href="/get-informed/guides/">How-To Guides</a>
-  <a href="/get-informed/recipes/">Recipes</a>
-  <h2><a href="/get-trained/">Get Trained</a></h2>
-  <h2><a href="/get-active/">Get Active</a></h2>
-  <a href="/get-active/activism/">Activism</a>
-  <a href="/get-active/churches/">Churches</a>
-  <a href="/get-active/community-events/">Community Events</a>
-  <a href="/get-active/memes/">Memes</a>
-  <a href="/get-active/small-group-resources/">Small Group Resources</a>
-  <h2><a href="/about/">About</a></h2>
+    <h2><a href="/get-informed/">Get Informed</a></h2>
+    <a href="/get-informed/books/">Books</a>
+    <a href="/get-informed/butchering/">Butchering</a>
+    <a href="/get-informed/food-preservation">Food Preservation</a>
+    <a href="/get-informed/gardening/">Gardening</a>
+    <a href="/get-informed/guides/">How-To Guides</a>
+    <a href="/get-informed/recipes/">Recipes</a>
+    <h2><a href="/get-trained/">Get Trained</a></h2>
+    <h2><a href="/get-active/">Get Active</a></h2>
+    <a href="/get-active/activism/">Activism</a>
+    <a href="/get-active/churches/">Churches</a>
+    <a href="/get-active/community-events/">Community Events</a>
+    <a href="/get-active/memes/">Memes</a>
+    <a href="/get-active/small-group-resources/">Small Group Resources</a>
+    <h2><a href="/about/">About</a></h2>
   </div>
   <div>
     <address>
       <a href="mailto:info@shelbyready.com">info@shelbyready.com</a>
     </address>
+    <p>
+      Website built and designed by
+      <a href="https://alexanderburdiss.com">Alexander Burdiss</a>
+    </p>
     <a href="/privacy-policy/">Privacy Policy</a>
   </div>
 </div>
--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
-// bundle.js created by bundlejs.sh v1.2.2 Sat Nov 19 11:32:50 EST 2022
+// bundle.js created by bundlejs.sh v1.2.2 Fri Nov 25 18:07:36 EST 2022
 // ./global.js
 (function globaljs() {// Add a JS class so the CSS can take into account the new JS styles.
 document.querySelector("body").classList.add("JS-Enabled");
@@ -247,30 +247,34 @@ if (tempPathParts.length > 0) {
 })();
 // ./assets/js/footer.js
 (function assetsjsfooterjs() {// Populate footer with links
-const footer = document.querySelector("footer");
+const footer = document.querySelector('footer');
 footer.innerHTML = `
 <div class="inner-content">
   <div class="left-container">
-  <h2><a href="/get-informed/">Get Informed</a></h2>
-  <a href="/get-informed/books/">Books</a>
-  <a href="/get-informed/butchering/">Butchering</a>
-  <a href="/get-informed/food-preservation">Food Preservation</a>
-  <a href="/get-informed/gardening/">Gardening</a>
-  <a href="/get-informed/guides/">How-To Guides</a>
-  <a href="/get-informed/recipes/">Recipes</a>
-  <h2><a href="/get-trained/">Get Trained</a></h2>
-  <h2><a href="/get-active/">Get Active</a></h2>
-  <a href="/get-active/activism/">Activism</a>
-  <a href="/get-active/churches/">Churches</a>
-  <a href="/get-active/community-events/">Community Events</a>
-  <a href="/get-active/memes/">Memes</a>
-  <a href="/get-active/small-group-resources/">Small Group Resources</a>
-  <h2><a href="/about/">About</a></h2>
+    <h2><a href="/get-informed/">Get Informed</a></h2>
+    <a href="/get-informed/books/">Books</a>
+    <a href="/get-informed/butchering/">Butchering</a>
+    <a href="/get-informed/food-preservation">Food Preservation</a>
+    <a href="/get-informed/gardening/">Gardening</a>
+    <a href="/get-informed/guides/">How-To Guides</a>
+    <a href="/get-informed/recipes/">Recipes</a>
+    <h2><a href="/get-trained/">Get Trained</a></h2>
+    <h2><a href="/get-active/">Get Active</a></h2>
+    <a href="/get-active/activism/">Activism</a>
+    <a href="/get-active/churches/">Churches</a>
+    <a href="/get-active/community-events/">Community Events</a>
+    <a href="/get-active/memes/">Memes</a>
+    <a href="/get-active/small-group-resources/">Small Group Resources</a>
+    <h2><a href="/about/">About</a></h2>
   </div>
   <div>
     <address>
       <a href="mailto:info@shelbyready.com">info@shelbyready.com</a>
     </address>
+    <p>
+      Website built and designed by
+      <a href="https://alexanderburdiss.com">Alexander Burdiss</a>
+    </p>
     <a href="/privacy-policy/">Privacy Policy</a>
   </div>
 </div>
