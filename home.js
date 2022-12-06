ab97e429d4a25a1701e3d080f08494ce23be97fb
Alexander Burdiss <aburdiss@icloud.com>
2022-12-05 19:00:13 -0500

    Update Iron Sharpens Iron Page
    - Changes TBD to TBA
    - Removes location
    - Adds meme to sign up for event
    - Updates JS to handle TBD as well as TBA

--- home.js
+++ home.js
@@ -1,8 +1,8 @@
 // bundle-ignore
 
-const upcomingEventContainer = document.querySelector(".js-upcoming-event");
+const upcomingEventContainer = document.querySelector('.js-upcoming-event');
 const today = new Date();
-fetch("/assets/calendar/calendar.json")
+fetch('/assets/calendar/calendar.json')
   .then((resp) => resp.json())
   .then(function (data) {
     let hasCurrentEventLoaded = false;
@@ -16,19 +16,21 @@ fetch("/assets/calendar/calendar.json")
         const eventDate = new Date(event.date);
         if (
           !hasCurrentEventLoaded &&
-          (eventDate.getTime() >= today.getTime() || event.date === "TBD")
+          (eventDate.getTime() >= today.getTime() ||
+            event.date === 'TBD' ||
+            event.date === 'TBA')
         ) {
-          const newNode = document.createElement("div");
+          const newNode = document.createElement('div');
           newNode.innerHTML = `
         <h3>${event.name}</h3>
         <div>Date: <em>${event.date}</em></div>
-        ${event.time ? `<div>Time: <em>${event.time}</em></div>` : ""}
-        ${event.location ? `<p>${event.location}</p>` : ""}
-        ${event.description ? `<p>${event.description}</p>` : ""}
+        ${event.time ? `<div>Time: <em>${event.time}</em></div>` : ''}
+        ${event.location ? `<p>${event.location}</p>` : ''}
+        ${event.description ? `<p>${event.description}</p>` : ''}
         ${
           event.link
             ? `<a href="${event.link}" class="button">Learn More</a>`
-            : ""
+            : ''
         }`;
           upcomingEventContainer.appendChild(newNode);
           hasCurrentEventLoaded = true;
